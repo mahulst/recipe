98e5229568ef977fd47ff3484673506833cd1990
mahulst <hulstmichel@gmail.com>
2014-07-22 21:43:46 +0200

    placed test on controller in stead of service

--- client/app/recepten/list/recepten.list.controller.spec.js
+++ client/app/recepten/list/recepten.list.controller.spec.js
@@ -5,10 +5,22 @@ describe('Controller: ReceptenListCtrl', function () {
   // load the controller's module
   beforeEach(module('recipeApp'));
 
-  var ReceptenListCtrl, scope;
+  var ReceptenListCtrl, scope, $httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, _$httpBackend_) {
+    var returnData;
+
+    //data for mock backend to return
+    returnData = [{
+        name: 'Recipe 1'
+      },{
+        name: 'Recipe 2'
+      },{
+        name: 'Recipe 3'
+      }];
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('/api/recipes').respond(returnData);
     scope = $rootScope.$new();
     ReceptenListCtrl = $controller('ReceptenListCtrl', {
       $scope: scope
@@ -16,6 +28,8 @@ describe('Controller: ReceptenListCtrl', function () {
   }));
 
   it('should add recipes to scope', function () {
+    $httpBackend.flush();
     expect(scope.recipes).to.be.an('array');
+    expect(scope.recipes.length).to.equal(3);
   });
 });
--- client/app/recepten/service/recept.service.spec.js
+++ client/app/recepten/service/recept.service.spec.js
@@ -11,21 +11,4 @@ describe('Service: recepten', function () {
     $httpBackend = _$httpBackend_;
     Recept = _Recept_;
   }));
-
-  it('should return list of recipes', function () {
-    var returnData = [{
-        name: 'Recipe 1'
-      },{
-        name: 'Recipe 2'
-      },{
-        name: 'Recipe 3'
-      }];
-    $httpBackend.expectGET('/api/recipes')
-      .respond(returnData);
-    var recipes = Recept.query();
-    $httpBackend.flush()
-    expect(recipes).to.be.an('array');
-    expect(recipes.length).to.equal(3);
-  });
-
 });
