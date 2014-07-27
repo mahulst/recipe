308c71e8f2dbf1c29452fe7c98ac1804e8db514c
mahulst <hulstmichel@gmail.com>
2014-07-27 18:18:35 +0200

    refactoring error displaying of serverside validation

--- client/components/form-from-model/formFromModel.directive.js
+++ client/components/form-from-model/formFromModel.directive.js
@@ -13,15 +13,19 @@ angular.module('recipeApp')
       	scope.ingredient = {};
       	scope.save = function (form) {
       		scope.submitted = true;
+          	//delete old errors
+          	angular.forEach(scope.errors, function(error, field) {
+				form[field].$setValidity('mongoose', true);
+			});
+          	scope.errors = {};
       		scope.saveFn({ingredient: scope.ingredient}).catch(function (err) {
-      			  err = err.data;
-		          scope.errors = {};
+      			err = err.data;
 
-		          // Update validity of form fields that match the mongoose errors
-		          angular.forEach(err.errors, function(error, field) {
-		            form[field].$setValidity('mongoose', false);
-		            scope.errors[field] = error.message;
-		          });
+				// Update validity of form fields that match the mongoose errors
+				angular.forEach(err.errors, function(error, field) {
+					form[field].$setValidity('mongoose', false);
+					scope.errors[field] = error.message;
+				});
       		});
       	};
       }
