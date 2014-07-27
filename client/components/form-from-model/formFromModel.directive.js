a4a936e0309c9c4c5f8b2e6933a56a1c53693e53
mahulst <hulstmichel@gmail.com>
2014-07-27 21:10:07 +0200

    refactoring 1.0 of formFromModel directive.

--- client/components/form-from-model/formFromModel.directive.js
+++ client/components/form-from-model/formFromModel.directive.js
@@ -15,8 +15,9 @@ angular.module('recipeApp')
       		scope.submitted = true;
           	//delete old errors
           	angular.forEach(scope.errors, function(error, field) {
-				form[field].$setValidity('mongoose', true);
-			});
+      				form[field].$setValidity('mongoose', true);
+              console.log(field+ " is valid")
+      			});
           	scope.errors = {};
       		scope.saveFn({ingredient: scope.ingredient}).catch(function (err) {
       			err = err.data;
@@ -24,6 +25,7 @@ angular.module('recipeApp')
 				// Update validity of form fields that match the mongoose errors
 				angular.forEach(err.errors, function(error, field) {
 					form[field].$setValidity('mongoose', false);
+              console.log(field+ " is invalid")
 					scope.errors[field] = error.message;
 				});
       		});
