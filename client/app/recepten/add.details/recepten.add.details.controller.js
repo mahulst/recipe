8e1841b03c408ae031e6095a52f62c96b520734a
mahulst <michel@touchtribe.nl>
2014-09-05 15:56:20 +0200

    detail controller automatically adds new step when last step is edited

--- client/app/recepten/add.details/recepten.add.details.controller.js
+++ client/app/recepten/add.details/recepten.add.details.controller.js
@@ -1,6 +1,30 @@
 'use strict';
 
 angular.module('recipeApp')
-  .controller('ReceptenAddDetailsCtrl', function () {
+  .controller('ReceptenAddDetailsCtrl', function ($scope) {
+    $scope.recipe = $scope.recipe || {};
+    if(!$scope.recipe.steps) {
+        $scope.recipe.steps = [];
+        $scope.lastEntry = {
+            id: 0,
+            text:'',
+            ingredients: []
+        };
+        $scope.recipe.steps.push($scope.lastEntry);
+    } else {
+        $scope.lastEntry = $scope.recipe.steps[$scope.recipe.steps.length - 1];
+    }
 
+
+    //adding a new step if last step is filled out
+    $scope.$watch('lastEntry.text', function (newVal, oldVal) {
+        if(newVal !== '') {
+            $scope.lastEntry = {
+                id: $scope.recipe.steps.length,
+                text: '',
+                ingredients: []
+            };
+            $scope.recipe.steps.push($scope.lastEntry);
+        }
+    });
   });
--- client/app/recepten/add.details/recepten.add.details.controller.spec.js
+++ client/app/recepten/add.details/recepten.add.details.controller.spec.js
@@ -5,17 +5,26 @@ describe('Controller: ReceptenAddDetailsCtrl', function () {
   // load the controller's module
   beforeEach(module('recipeApp'));
 
-    var ReceptenAddDetailsCtrl, scope;
-
+    var $rootScope, $httpBackend, createController, $controller;
+    beforeEach(inject(function ($injector) {
+        $controller = $injector.get('$controller');
+        $httpBackend = $injector.get('$httpBackend');
+        $rootScope = $injector.get('$rootScope');
+        createController = function () {
+            return $controller('ReceptenAddDetailsCtrl', {$scope: $rootScope});
+        };
+    }));
     // Initialize the controller and a mock scope
-    beforeEach(inject(function ($controller, $rootScope) {
-        scope = $rootScope.$new();
-        ReceptenAddDetailsCtrl = $controller('ReceptenAddDetailsCtrl', {
-            $scope: scope
-        });
+    beforeEach(inject(function () {
+//        var controller = createController();
     }));
 
-    it('should ...', function () {
-        expect(1).to.equal(1);
+    it('should create new step if last step is filled', function () {
+        var controller = createController();
+        expect($rootScope.recipe.steps.length).to.equal(1);
+        $rootScope.lastEntry.text = 'Test';
+        $rootScope.$apply();
+        expect($rootScope.recipe.steps.length).to.equal(2);
+
     });
 });
