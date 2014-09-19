b5f25af8cce81b410b2eaab4a0a4f4cf1dfa14ff
mahulst <michel@touchtribe.nl>
2014-09-19 09:40:00 +0200

    string replacemnet changed

--- client/components/filters.js
+++ client/components/filters.js
@@ -2,7 +2,7 @@ angular.module('recipeApp')
     .filter('printf', function() {
         return function(input, arr) {
             return input.replace(/\{(\d+)\}/g, function (match, capture) {
-                return arr[1*capture].name;
+                return '[~' + arr[1*capture].name +']';
             });
         };
     });
