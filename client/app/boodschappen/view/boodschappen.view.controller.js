53ef858187f02931331e520ff4388eb24a96ae09
mahulst <michel@touchtribe.nl>
2014-09-26 16:17:41 +0200

    grocery listing ingredients based on recipe

--- client/app/boodschappen/view/boodschappen.view.controller.js
+++ client/app/boodschappen/view/boodschappen.view.controller.js
@@ -8,4 +8,22 @@ angular.module('recipeApp')
     	$scope.showDetail = true;
     }
     $scope.currentList = BoodschappenLijst.getList();
+    $scope.ingredients = listIngredients();
+    function listIngredients() {
+        var obj = {};
+        angular.forEach($scope.currentList.recepten, function (recept) {
+            angular.forEach(recept.ingredients, function (ingredient){
+                if(!ingredient) {
+                    return;
+                }
+                console.log(ingredient);
+                if(!obj[ingredient.ingredient._id]){
+                    obj[ingredient.ingredient._id] = {ingredient: ingredient.ingredient, amount: ingredient.amount};
+                } else {
+                    obj[ingredient.ingredient._id].amount += ingredient.amount;
+                }
+            });
+        });
+        return obj;
+    };
   });
--- server/config/socketio.js
+++ server/config/socketio.js
@@ -18,6 +18,7 @@ function onConnect(socket) {
   });
 
   // Insert sockets below
+  require('../api/grocery-list/grocery-list.socket').register(socket);
   require('../api/tag/tag.socket').register(socket);
   require('../api/ingredient/ingredient.socket').register(socket);
   require('../api/recipe/recipe.socket').register(socket);
