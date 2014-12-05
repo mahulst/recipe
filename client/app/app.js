5af1edbb310d70049cc53560c4279a2f6a677f2a
mahulst <michel@touchtribe.nl>
2014-12-05 15:08:08 +0100

    main page html

--- client/app/app.js
+++ client/app/app.js
@@ -7,7 +7,8 @@ angular.module('recipeApp', [
   'ngSanitize',
   'ui.bootstrap',
   'btford.socket-io',
-  'ui.router'
+  'ui.router',
+  'angular-carousel'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,5 +2,42 @@
 
 angular.module('recipeApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.message = 'Hello';
+    $scope.myInterval = 5000;
+    var slides = $scope.slides = [{
+      image: 'assets/images/recipe1.jpg',
+      text: 'Recept 1',
+      id: 1
+    },
+    {
+      image: 'assets/images/recipe2.jpg',
+      text: 'Recept 2',
+      id: 2
+    },
+    {
+      image: 'assets/images/recipe3.jpg',
+      text: 'Recept 3',
+      id: 3
+    }];
+
+
+    $scope.bestRecipes =[{
+      name: 'test 1',
+      rating: 'rating3'
+    },{
+      name: 'test 2',
+      rating: 'rating2'
+    },{
+      name: 'test 3',
+      rating: 'rating5'
+    },{
+      name: 'test 4',
+      rating: 'rating1'
+    },{
+      name: 'test 5',
+      rating: 'rating4'
+    },{
+      name: 'test 6',
+      rating: 'rating3'
+    },
+    ];
   });
